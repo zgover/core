7b85d5f667302e9619eb0248dc1b5d2bb8eac1d4
Zach Gover <9044773+zgover@users.noreply.github.com>
2026-05-30 01:05:31 -0500

    fix: import error in shared-util-rest-api

--- libs/shared/util/rest-api/src/lib/http-request-method-middleware.ts
+++ libs/shared/util/rest-api/src/lib/http-request-method-middleware.ts
@@ -1,6 +1,6 @@
 /**
  * @license
- * Copyright 2022 Aglyn LLC
+ * Copyright 2026 Aglyn LLC
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
@@ -26,8 +26,9 @@ import {
   HttpStatusCode,
 } from '@aglyn/shared-data-enums'
 import { HttpResponseError } from '@aglyn/shared-util-errors'
-import type { NextMiddleware } from 'next-api-middleware'
+import type { Middleware as NextMiddleware } from 'next-api-middleware'
 import nextHandleJsonResponse from './next-handle-json-response'
+
 
 /**
  * @example
